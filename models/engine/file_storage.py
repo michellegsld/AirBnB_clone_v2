ea16cb4d6687a49501db04643ab1011a3ac5a5c1
KFredlund <katihomes@gmail.com>
2020-03-29 18:56:16 +0000

    Update file storage to delete obj if it's inside

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,7 +20,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
@@ -55,3 +55,10 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """deletes obj from __objects if it's inside
+        """
+        if obj:
+            del self.__objects[obj]
+        return self.__objects
