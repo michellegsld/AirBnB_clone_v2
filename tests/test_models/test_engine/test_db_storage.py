9b8a33e8e427f9339b3c5503a75a3f443916d36f
KFredlund <katihomes@gmail.com>
2020-03-30 23:15:26 +0000

    [New] Create a unittest template for DBStorage

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Tests for DBStorge """
+
+import unittest
+from models.engine.db_storage import DBStorage
+import os
+
+
+class TestDBStorage(unittest.TestCase):
+    """ DBStorage Tests """
+
+    def test_all(self):
+        """Tests all method """
+        pass
+
+    def test_new(self):
+        """Tests new method """
+
+        pass
+
+    def test_delete(self):
+        """Tests delete method """
+
+        pass
+
+    def test_reload(self):
+        """Tests reload method """
+
+        pass
