721d5b9545f610ecb01b2c3f579febc080119987
KFredlund <katihomes@gmail.com>
2020-03-30 12:34:54 +0000

    [Update] Represent attributes in columns

--- models/place.py
+++ models/place.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """This is the place class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
+        __tablename__: table places
         city_id: city id
         user_id: user id
         name: name input
@@ -18,14 +22,19 @@ class Place(BaseModel):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+    city_id = Column("city_id", String(60),
+                     ForeignKey("cities.id"), nullable=False)
+    user_id = Column("user_id", String(60),
+                     ForeignKey("users.id"), nullable=False)
+    name = Column("name", String(128), nullable=False)
+    description = Column("description", String(1024), nullable=True)
+    number_rooms = Column("number_rooms", Integer, default=0, nullable=False)
+    number_bathrooms = Column("number_bathrooms",
+                              Integer, default=0, nullable=False)
+    max_guest = Column("max_guest", Integer, default=0, nullable=False)
+    price_by_night = Column("price_by_night",
+                            Integer, default=0, nullable=False)
+    latitude = Column("latitude", Float, nullable=True)
+    longitude = Column("longitude", Float, nullable=True)
     amenity_ids = []
