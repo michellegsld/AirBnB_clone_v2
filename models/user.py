b0f6e90f7704ada30c7929b818a08b0ad677d7be
KFredlund <katihomes@gmail.com>
2020-03-30 12:02:53 +0000

    Update User to inherit from Base as well and represents attributes in columns

--- models/user.py
+++ models/user.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
-        password: password for you login
+        password: password for your login
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column("email", String(128), nullable=False)
+    password = Column("password", String(128), nullable=False)
+    first_name = Column("first_name", String(128), nullable=True)
+    last_name = Column("last_name", String(128), nullable=True)
