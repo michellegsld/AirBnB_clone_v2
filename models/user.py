e0bc1f34b91c9f63e06bb211b406933ecb6f15f2
KFredlund <katihomes@gmail.com>
2020-03-30 12:54:27 +0000

    [Update] Create a relationship between Review and Place for both DBStorage and FileStorage

--- models/user.py
+++ models/user.py
@@ -22,3 +22,4 @@ class User(BaseModel, Base):
     first_name = Column("first_name", String(128), nullable=True)
     last_name = Column("last_name", String(128), nullable=True)
     places = relationship('Place', backref='user')
+    reviews = relationship('Review', backref='user')
