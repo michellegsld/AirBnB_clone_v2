869bab57d417a50ca5a7e13a898c456bd6bf55d1
Michelle Giraldo <michellegsld@gmail.com>
2020-03-31 19:49:24 -0400

    [Update] Change storage to be DB so class attributes are now columns

--- models/state.py
+++ models/state.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state')
+
+    @property
+    def cities(self):
+        """
+        Returns the list of City instances where
+        state_id equals the current State.id
+        """
+        city_list = []
+        city_dict = storage.all(City)
+        for key, value in city_dict.items():
+            city_list.append(city_dict[key])
+        return city_list
