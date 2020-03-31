9cfda20a7f8cb2ede82fce08d458c1356bb13e72
KFredlund <katihomes@gmail.com>
2020-03-31 00:52:23 +0000

    [Update] Add more tests to template

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -4,6 +4,7 @@
 import unittest
 from models.engine.db_storage import DBStorage
 import os
+from sqlalchemy.sql import select
 
 
 class TestDBStorage(unittest.TestCase):
@@ -25,5 +26,35 @@ class TestDBStorage(unittest.TestCase):
 
     def test_reload(self):
         """Tests reload method """
+
+        pass
+
+    def test_place_amenity_table(self):
+        """Tests place_amenity table """
+
+        pass
+
+    def test_column_cities(self):
+        """Tests cities column  """
+
+        pass
+
+    def test_relationship_city_state(self):
+        """Tests relationship between city and state """
+
+        pass
+
+    def test_drop(self):
+        """Tests if all tables were dropped if HBNB_ENV == test """
+
+        pass
+
+    def test_relationship_place_user(self):
+        """Tests relationship between place and user """
+
+        pass
+
+    def test_relationship_places_city(self):
+        """Tests relationship between places and city"""
 
         pass
