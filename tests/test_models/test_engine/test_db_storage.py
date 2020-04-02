c57e4c874109b77297f2310965337bb044743011
Michelle Giraldo <michellegsld@gmail.com>
2020-04-02 16:19:22 -0400

    [Undo] Place pep8 test back and remove imports

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -4,19 +4,17 @@
 import unittest
 from models.engine.db_storage import DBStorage
 import os
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.engine.db_storage import DBStorage
 from sqlalchemy.sql import select
 
 
 class TestDBStorage(unittest.TestCase):
     """ DBStorage Tests """
+
+    def test_pep8_DBStorage(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_all(self):
         """Tests all method """
