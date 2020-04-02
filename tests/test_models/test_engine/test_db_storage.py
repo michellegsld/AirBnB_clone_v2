0401b72261a491ce4b3bb57970224cc4b318791a
Michelle Giraldo <michellegsld@gmail.com>
2020-04-02 16:17:46 -0400

    [Undo] Remove test of pep8

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -18,12 +18,6 @@ from sqlalchemy.sql import select
 class TestDBStorage(unittest.TestCase):
     """ DBStorage Tests """
 
-    def test_pep8_DBStorage(self):
-        """Tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
     def test_all(self):
         """Tests all method """
         pass
