2e82b6340e3b482b2670ce456591afa3e3fae222
Michelle Giraldo <michellegsld@gmail.com>
2020-04-06 21:17:43 -0400

    [Update] Remove group specifications for chown

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/ && sudo mkdir -p /data/web_static/shared/
 echo -e '<!DOCTYPE html>'"\n<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu: /data/
 var="\n\tlocation = /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "50i \ $var" /etc/nginx/sites-available/default
 sudo service nginx start
