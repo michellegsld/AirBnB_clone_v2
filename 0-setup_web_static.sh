93c8315fcb888ca14082a90a9afdee4c4384d7cb
Michelle Giraldo <michellegsld@gmail.com>
2020-04-06 20:45:50 -0400

    [Update] Add forgotten command tee and remove internal as unsure what it's for

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,9 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/ && sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
-echo -e '<!DOCTYPE html>'"\n<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" | sudo /data/web_static/releases/test/index.html
+echo -e '<!DOCTYPE html>'"\n<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
-var="\n\tlocation = /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tinternal;\n\t}"
+var="\n\tlocation = /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "50i \ $var" /etc/nginx/sites-available/default
 sudo service nginx start
