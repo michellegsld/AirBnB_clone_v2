3b6c0ab7e5b1872c6677286fe227b38458cea2d0
Michelle Giraldo <michellegsld@gmail.com>
2020-04-06 21:29:25 -0400

    [Update] Revert change with nginx service, remove the = when creating location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,10 @@
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test/ && sudo mkdir -p /data/web_static/shared/
 echo -e '<!DOCTYPE html>'"\n<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-var="\n\tlocation = /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+var="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "50i \ $var" /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo service nginx start
