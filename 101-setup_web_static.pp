e77a63b9db240ef290e756cc5787355f21f059f0
Michelle Giraldo <michellegsld@gmail.com>
2020-04-06 22:33:14 -0400

    [Fix] Just removing the DOCTYPE causing problems

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,5 +1,5 @@
 # Exact copy of task 0 but done as puppet, each line of task 0 is within command
 exec {'command':
-    command => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo mkdir -p /data/web_static/releases/test/ && sudo mkdir -p /data/web_static/shared/ && echo -e ''<!DOCTYPE html>''"\n<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && var="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" && sudo sed -i "50i \ $var" /etc/nginx/sites-available/default && sudo service nginx start',
+    command => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo mkdir -p /data/web_static/releases/test/ && sudo mkdir -p /data/web_static/shared/ && echo -e "\n<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && var="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" && sudo sed -i "50i \ $var" /etc/nginx/sites-available/default && sudo service nginx start',
     provider => shell,
 }
