07c9384bb3a851ee92affa25a2092fb3d96af12f
Michelle Giraldo <michellegsld@gmail.com>
2020-04-20 02:47:18 -0400

    [Initial] Create route that prints C and whatever text is inputed

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+2-c_route.py
+Task 2:
+A script that starts a Flask web application
+"""
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index(strict_slashes=False):
+    """
+    Main route and what it displays
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb(strict_slashes=False):
+    """
+    Route that displays HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text, strict_slashes=False):
+    """
+    Route that displays HBNB
+    """
+    return 'C {}'.format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
