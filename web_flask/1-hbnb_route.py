c9594ab79a21a38f81c925877347d4684b51a48e
Michelle Giraldo <michellegsld@gmail.com>
2020-04-20 02:29:54 -0400

    [Initial] Add to previous task, create task for hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+1-hbnb_route.py
+Task 1:
+A script that starts a Flask web application
+"""
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index(strict_slashes=False):
+    """
+    Main route and what it displays
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb(strict_slashes=False):
+    """
+    Route that displays HBNB
+    """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
