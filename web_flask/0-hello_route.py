12484e7f29a730d44c0eca4e55b72268ba7828a8
Michelle Giraldo <michellegsld@gmail.com>
2020-04-20 02:05:05 -0400

    [Initial] Create flash web application that has specific host and port, return sentence

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0-hello_route.py
+Task 0:
+A script that starts a Flask web application
+"""
+from flask import Flask, escape, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index(strict_slashes=False):
+    """
+    Main route and what it displays
+    """
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
