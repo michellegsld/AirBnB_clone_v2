ebbfb64e596ae76a89899541a26cb9a2ac274d98
Michelle Giraldo <michellegsld@gmail.com>
2020-04-21 15:02:58 -0400

    [Fix] Use .format instead of adding strings together

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -74,9 +74,9 @@ def number_odd_or_even(n, strict_slashes=False):
     if n.isdigit():
         file = '6-number_odd_or_even.html'
         if int(n) % 2 == 0:
-            string = n + " is even"
+            string = "{} is even".format(n)
         else:
-            string = n + " is odd"
+            string = "{} is odd".format(n)
         return render_template(file, string=string)
     abort(404)
 
