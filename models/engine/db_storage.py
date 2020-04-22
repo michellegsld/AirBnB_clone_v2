33ba15b1ef4ff86ead8c5a92cf00c8b473b2f3d8
Michelle Giraldo <michellegsld@gmail.com>
2020-04-21 20:38:42 -0400

    [Update] Create function called close

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -79,3 +79,8 @@ class DBStorage:
         session_factory = sessionmaker(self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Closes the session
+        """
+        self.__session.close()
