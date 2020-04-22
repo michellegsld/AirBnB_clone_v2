a3f57b84152899fe28acfae70f5898b6b2bcd09b
Michelle Giraldo <michellegsld@gmail.com>
2020-04-22 16:04:43 -0400

    [Update] Add if-statement fixing what to set cities to depending on the type of storage used

--- models/state.py
+++ models/state.py
@@ -16,17 +16,19 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state')
 
-    @property
-    def cities(self):
-        """
-        Returns the list of City instances where
-        state_id equals the current State.id
-        """
-        city_list = []
-        city_dict = models.storage.all(City)
-        for key, value in city_dict.items():
-            if city_dict[key].state_id == self.id:
-                city_list.append(city_dict[key])
-        return city_list
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship('City', backref='state')
+    else:
+        @property
+        def cities(self):
+            """
+            Returns the list of City instances where
+            state_id equals the current State.id
+            """
+            city_list = []
+            city_dict = models.storage.all(City)
+            for key, value in city_dict.items():
+                if city_dict[key].state_id == self.id:
+                    city_list.append(city_dict[key])
+            return city_list
