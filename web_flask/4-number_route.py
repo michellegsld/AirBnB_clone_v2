8293638168405e97aab9cb2ae02a8497e06fabd1
Michelle Giraldo <michellegsld@gmail.com>
2020-05-27 11:02:09 -0400

    [Fix] Change location of strict_slashes

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -9,42 +9,41 @@ from flask import Flask, abort
 app = Flask(__name__)
 
 
-@app.route('/')
-def index(strict_slashes=False):
+@app.route('/',strict_slashes=False)
+def index():
     """
     Main route and what it displays
     """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
-def hbnb(strict_slashes=False):
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     """
     Route that displays HBNB
     """
     return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_text(text, strict_slashes=False):
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
     """
     Route that displays C + <text>
     """
     return 'C {}'.format(text.replace("_", " "))
 
 
-@app.route('/python')
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_text(text='is cool', strict_slashes=False):
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
     """
     Route that displays Python + <text>
     """
     return 'Python {}'.format(text.replace("_", " "))
 
 
-@app.route('/number/<n>')
-def number_n(n, strict_slashes=False):
+@app.route('/number/<n>', strict_slashes=False)
+def number_n(n):
     """
     Route that displays <n> + is a number
     But only if <n> is an integer
